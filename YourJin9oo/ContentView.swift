df550a2f0f0e12dad5b331d4100a9c1e5853340b
min-gyoung <alsrud0205@naver.com>
2025-05-10 15:31:00 +0900

    Initial Commit

---
+++ YourJin9oo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YourJin9oo
+//
+//  Created by 김민경 on 5/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YourJin9oo/YourJin9ooApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourJin9ooApp.swift
+//  YourJin9oo
+//
+//  Created by 김민경 on 5/10/25.
+//
+
+import SwiftUI
+
+@main
+struct YourJin9ooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
