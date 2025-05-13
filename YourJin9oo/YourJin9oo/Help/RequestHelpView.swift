69fc13850781677e15018774242740653c9f6c64
min-gyoung <alsrud0205@naver.com>
2025-05-13 23:57:37 +0900

    feat: 다크모드 끔

---
+++ YourJin9oo/YourJin9oo/Help/RequestHelpView.swift
@@ -0,0 +1,92 @@
+//
+//  RequestHelpView.swift
+//  YourJin9oo
+//
+//  Created by 김민경 on 5/13/25.
+//
+
+import SwiftUI
+
+struct RequestHelpView: View {
+
+    var body: some View {
+        VStack {
+            Text("도움 요청")
+                .font(.system(size: 28))
+                .fontWeight(.bold)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 20)
+                .padding(.bottom, 16)
+            
+            Text("🚨 매칭 대기중...")
+                .font(.system(size: 20))
+                .fontWeight(.bold)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 20)
+        
+            ZStack(alignment: .leading) {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color.gray)
+                    .frame(height: 140)
+                    .padding(.leading, 20)
+                    .padding(.trailing, 20)
+                VStack{
+                    HStack {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 4)
+                                .fill(Color.pink)
+                                .frame(width: 54, height: 20)
+                            
+                            Text("매칭중")
+                                .font(.system(size: 14))
+                                .foregroundColor(.white)
+                        }
+                        .padding(.leading, 40)
+                        
+                        Text("심심한 사람?")
+                            .font(.system(size: 16))
+                            .fontWeight(.semibold)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 5)
+                        
+                        Text("n/n")
+                            .font(.system(size: 16))
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                            .padding(.trailing, 40)
+                    }
+                    .padding(.bottom, 5)
+                    
+                    Text("저랑 마크에서 1대1 뜰사람 찾아요\n대신 맨주먹으로 저이기면 특별히 아끼는 ...")
+                        .font(.system(size: 16))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 40)
+                        .padding(.bottom, 3)
+                    
+                    HStack {
+                        Text("1일 전 • 조회 999")
+                            .font(.system(size: 16))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 40)
+                        
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color.black)
+                                .frame(width: 60, height: 24)
+                            
+                            Text("스포츠")
+                                .font(.system(size: 14))
+                                .foregroundColor(.white)
+                        }
+                        .padding(.trailing, 40)
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+#Preview {
+    RequestHelpView()
+}
