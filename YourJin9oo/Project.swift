6dd6a5256c885bd0513e2d2f4605612e71a568b4
Hong <dbsghdz1@gmail.com>
2025-05-13 12:07:23 +0900

    chore: tusit로 변경

---
+++ YourJin9oo/Project.swift
@@ -0,0 +1,34 @@
+import ProjectDescription
+
+let project = Project(
+    name: "YourJin9oo",
+    targets: [
+        .target(
+            name: "YourJin9oo",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "io.tuist.YourJin9oo",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchScreen": [
+                        "UIColorName": "",
+                        "UIImageName": "",
+                    ],
+                ]
+            ),
+            sources: ["YourJin9oo/Sources/**"],
+            resources: ["YourJin9oo/Resources/**"],
+            dependencies: []
+        ),
+        .target(
+            name: "YourJin9ooTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "io.tuist.YourJin9ooTests",
+            infoPlist: .default,
+            sources: ["YourJin9oo/Tests/**"],
+            resources: [],
+            dependencies: [.target(name: "YourJin9oo")]
+        ),
+    ]
+)
