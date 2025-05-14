5c3396374d97d3e8e25d4ab1c102ec70e424155d
min-gyoung <alsrud0205@naver.com>
2025-05-14 14:38:52 +0900

    feat: 도움 주기 페이지 상단 부분 세팅

--- YourJin9oo/YourJin9oo/Help/GiveHelpView.swift
+++ YourJin9oo/YourJin9oo/Help/GiveHelpView.swift
@@ -8,8 +8,69 @@
 import SwiftUI
 
 public struct GiveHelpView: View {
+    let categories = ["전체", "디자인", "개발", "스포츠"]
+
     public var body: some View {
-        Text("Give Help")
+        VStack {
+            Text("도움 주기")
+                .font(.system(size: 28, weight: .bold))
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 20)
+                .padding(.bottom, 16)
+                .padding(.top, 20)
+            
+            HStack {
+                ForEach(categories, id: \.self) { category in
+                    Button(action:{
+                        handleCategorySelection(category)
+                    }) {
+                        Spacer()
+                        
+                        Text(category)
+                            .font(.system(size: 16))
+                            .foregroundColor(.black)
+                            .frame(maxWidth: .infinity, minHeight: 34)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 20)
+                                    .stroke(Color.gray, lineWidth: 1)
+                            )
+                            .cornerRadius(20)
+                    }
+                }
+            }
+            .padding(.leading, 10)
+            .padding(.trailing, 10)
+        }
+    }
+    
+    // 카테고리 선택
+    private func handleCategorySelection(_ category: String) {
+        switch category {
+        case "전체":
+            handleAll()
+        case "디자인":
+            handleDesign()
+        case "개발":
+            handleDev()
+        case "스포츠":
+            handleSports()
+        default:
+            handleAll()
+        }
+    }
+    
+    // 카테고리 함수 처리
+    private func handleAll() {
+        
+    }
+    private func handleDesign() {
+        
+    }
+    private func handleDev() {
+        
+    }
+    private func handleSports() {
+        
     }
 }
 
