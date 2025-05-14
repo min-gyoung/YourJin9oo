e163300008a7f376411348d6073201bd5cf9f3d9
min-gyoung <alsrud0205@naver.com>
2025-05-14 14:41:00 +0900

    feat: 도움 주기 글 예시

--- YourJin9oo/YourJin9oo/Help/GiveHelpView.swift
+++ YourJin9oo/YourJin9oo/Help/GiveHelpView.swift
@@ -40,6 +40,60 @@ public struct GiveHelpView: View {
             }
             .padding(.leading, 10)
             .padding(.trailing, 10)
+            .padding(.bottom, 20)
+            
+            ZStack(alignment: .leading) {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color(hex: "#F3F3F3"))
+                    .stroke(Color(hex: "#E9E9E9"), lineWidth: 1)
+                    .frame(height: 140)
+                    .padding(.leading, 20)
+                    .padding(.trailing, 20)
+                VStack {
+                    HStack {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 4)
+                                .fill(Color(hex: "#A6A6A6"))
+                                .frame(width: 54, height: 20)
+                            
+                            Text("매칭완")
+                                .font(.system(size: 14))
+                                .foregroundColor(.white)
+                        }
+                        .padding(.leading, 40)
+                        
+                        Text("심심한 사람?")
+                            .font(.system(size: 16, weight: .semibold))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 5)
+                    }
+                    .padding(.bottom, 5)
+                    
+                    Text("저랑 마크에서 1대1 뜰사람 찾아요\n대신 맨주먹으로 저이기면 특별히 아끼는 ...")
+                        .font(.system(size: 16))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 40)
+                        .padding(.bottom, 3)
+                    
+                    HStack {
+                        Text("1일 전 • 조회 999")
+                            .font(.system(size: 16))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 40)
+                        
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color(hex: "#DBDBDB"))
+                                .frame(width: 60, height: 24)
+                            
+                            Text("스포츠")
+                                .font(.system(size: 14))
+                                .foregroundColor(Color(hex: "#666666"))
+                        }
+                        .padding(.trailing, 40)
+                    }
+                }
+            }
         }
     }
     
