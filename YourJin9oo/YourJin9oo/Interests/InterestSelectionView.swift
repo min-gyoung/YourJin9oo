152f6319971ae4681b2ce784a0e0763780156afe
min-gyoung <alsrud0205@naver.com>
2025-05-15 22:11:58 +0900

    feat: Interest 버튼 삭제

--- YourJin9oo/YourJin9oo/Interests/InterestSelectionView.swift
+++ YourJin9oo/YourJin9oo/Interests/InterestSelectionView.swift
@@ -14,11 +14,36 @@ public struct InterestSelectionView: View {
             .fontWeight(.bold)
             .foregroundColor(Color(hex: "#666666"))
             .padding(.bottom, 10)
-
+            .padding(.top, 100)
+        
         Text("다른 사람에게 도움 줄 수 있는\n분야를 골라주세요.")
             .font(.system(size: 14))
             .foregroundColor(Color(hex: "#666666"))
             .multilineTextAlignment(.center)
+        
+        Spacer()
+        Button(action: {
+            // 누르면 버튼이 사라져야 함
+        } ) {
+            ZStack {
+                RoundedRectangle(cornerRadius: 16)
+                    .fill(Color.clear)
+                    .stroke(Color(hex: "#666666"))
+                    .frame(width: 90, height: 30)
+                
+                HStack{
+                    Text("스포츠")
+                        .font(.system(size: 16))
+                        .foregroundColor(Color(hex: "#666666"))
+                    
+                    Image(systemName: "xmark")
+                        .font(.system(size: 14))
+                        .foregroundColor(Color(hex: "#666666"))
+                }
+                
+            }
+        }
+        Spacer()
     }
 }
 
