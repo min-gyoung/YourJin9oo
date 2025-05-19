1c13980872c6a06483cd009e8404b7c1ecd1d191
min-gyoung <alsrud0205@naver.com>
2025-05-19 14:17:10 +0900

    feat: 관심 분야 선택 뷰 버튼 변경

--- YourJin9oo/YourJin9oo/Sources/Interests/InterestSelectionView.swift
+++ YourJin9oo/YourJin9oo/Sources/Interests/InterestSelectionView.swift
@@ -8,36 +8,57 @@
 import SwiftUI
 
 public struct InterestSelectionView: View {
-    let interestList = ["스포츠", "맛집", "생활", "배움", "취미", "건강"]
+    let interestList: [(name: String, icon: String)] = [
+        ("스포츠", "dumbbell.fill"),
+        ("맛집", "frying.pan.fill"),
+        ("생활", "leaf.fill"),
+        ("배움", "books.vertical.fill"),
+        ("취미", "music.note"),
+        ("건강", "heart.fill")
+    ]
     
     public var body: some View {
         Text("당신의 관심분야는 뭔가요?")
-            .font(.system(size: 24))
-            .fontWeight(.bold)
-//            .foregroundColor(Color(hex: "#666666"))
+            .font(.system(size: 24, weight: .bold))
+        //            .foregroundColor(Color(hex: "#666666"))
             .padding(.bottom, 10)
-            .padding(.top, 100)
+            .padding(.top, 80)
         
         Text("다른 사람에게 도움 줄 수 있는\n분야를 골라주세요.")
             .font(.system(size: 14))
-//            .foregroundColor(Color(hex: "#666666"))
+        //            .foregroundColor(Color(hex: "#666666"))
             .multilineTextAlignment(.center)
         
         Spacer()
         
         LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: -60), count: 2), spacing: 15) {
-            ForEach(interestList, id: \.self) { interest in
+            ForEach(interestList, id: \.name) { interest in
                 Button(action: {
-                    print("\(interest)")
+                    print("\(interest.name)")
                 } ) {
                     VStack {
-                        RoundedRectangle (cornerRadius: 16)
-//                            .fill(Color(hex: "#D9D9D9"))
-                            .frame(width: 150, height: 150)
-                            .overlay(
-                                Text(interest)
-                                    .foregroundColor(.black)
-                            )
+                        ZStack {
+                            RoundedRectangle (cornerRadius: 16)
+                                .fill(Color.gray)
+                            //                            .fill(Color(hex: "#D9D9D9"))
+                                .frame(width: 150, height: 150)
+                            //                            .overlay(
+                            //                                Text(interest)
+                            //                                    .foregroundColor(.black)
+                            //                            )
+                            
+                            VStack {
+                                Image(systemName: interest.icon)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .foregroundColor(Color.white)
+                                    .frame(width: 60, height: 60)
+                                
+                                Text(interest.name)
+                                    .font(.system(size: 20, weight: .bold))
+                                    .foregroundColor(.white)
+                            }
+                        }
                     }
                 }
             }
