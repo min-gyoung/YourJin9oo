0b347aa95473fe5462099fa9657018a6f7b7e362
min-gyoung <alsrud0205@naver.com>
2025-05-22 10:08:28 +0900

    chore: hex to asset

--- YourJin9oo/YourJin9oo/Sources/YourJin9ooApp.swift
+++ YourJin9oo/YourJin9oo/Sources/YourJin9ooApp.swift
@@ -1,17 +1,17 @@
 import SwiftUI
+import FirebaseCore
 
-//
-//class AppDelegate: NSObject, UIApplicationDelegate {
-//    func application(_ application: UIApplication,
-//                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-//        FirebaseApp.configure()
-//        return true
-//    }
-//}
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct YourJin9ooApp: App {
-//    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
         WindowGroup {
