ce2f826e7c37bdf68d97a14c8c59ce844e188e89
min-gyoung <alsrud0205@naver.com>
2025-05-22 21:25:54 +0900

    fix: error

--- YourJin9oo/YourJin9oo/Sources/ContentView.swift
+++ YourJin9oo/YourJin9oo/Sources/ContentView.swift
@@ -4,13 +4,9 @@ public struct ContentView: View {
     public init() {}
 
     public var body: some View {
-<<<<<<< HEAD
         VStack {
             Text("Hello, World!")
         }
-=======
-//        TabBarView()
->>>>>>> main
     }
 }
 
